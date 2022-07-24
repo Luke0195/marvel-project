41c495c132fb680e5cdbc32c2df66e5e345a9a4a
Lucas <lucas201273@gmail.com>
2022-07-23 22:08:49 -0300

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-const App = ():JSX.Element =>{
-    return(
-      <h1> Marvel Project </h1>
-    )
+const App = (): JSX.Element => {
+  return <h1> Marvel Project </h1>
 }
 
-export default App;
+export default App
