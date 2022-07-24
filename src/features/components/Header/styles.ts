17efffaf81eec52ab1453c0de6561cac4e857385
Lucas <lucas201273@gmail.com>
2022-07-24 10:09:15 -0300

    feat: styles header component

--- src/features/components/Header/styles.ts
+++ src/features/components/Header/styles.ts
@@ -10,8 +10,32 @@ export const Content = styled.div`
   width: 100%;
   max-width: 82.5rem;
   margin: auto;
+  padding: 2.5rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 
-export const LeftContent = styled.div``
+export const LeftContent = styled.div`
+  flex: 1;
+`
 
-export const RigthContent = styled.div``
+export const RigthContent = styled.div`
+  flex: 1;
+  ul {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+
+    li {
+      padding: 0 1rem;
+      color: ${({ theme }) => theme.colors.white};
+      cursor: pointer;
+      transition: filter 0.6s ease-out;
+      &:hover {
+        filter: brightness(60%);
+      }
+    }
+  }
+`
